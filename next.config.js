77ae55edab61e4a3785e26b5029d87ef0c007d4e
slav1028 <359mstgroup@gmail.com>
2025-07-13 14:22:01 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'], // Добави домейни тук ако използваш <Image src="...">
+  },
+};
+
+module.exports = nextConfig;
